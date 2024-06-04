6019eab4603f6c00285dae0522c31c5fc7e6af6e
INGANZAMARUMPU <jonkur96@gmail.com>
2024-06-04 17:21:43 +0200

    fetch themes

--- src/views/Thematique.vue
+++ src/views/Thematique.vue
@@ -9,9 +9,20 @@
       </ion-toolbar>
     </ion-header>
     <ion-content class="ion-no-padding">
-      <div v-if="!theme" class="chooser">
+      <div v-if="!theme" class="chooser" @click="selectMap">
         <div>Choisissez un theme pour voir la carte</div>
-        <ion-button>Choisir le thème</ion-button>
+        <ion-button>
+          Choisir le thème
+        </ion-button>
+        <ion-select
+          id="map_chooser"
+          multiple="false"
+          @IonChange="theme = $event.target.value"
+          cancel-text="reka" ok-text="sawa">
+          <ion-select-option v-for="item in themes" :value="item">
+            {{ item.theme }}
+          </ion-select-option>
+        </ion-select>
       </div>
       <div v-else>
 
@@ -25,12 +36,28 @@ export default {
   data(){
     return {
       theme: null,
-      themes: []
+      themes: this.$store.state.themes
+    }
+  },
+  watch:{
+    "$store.state.themes"(new_val){
+      this.themes = new_val
     }
   },
   methods:{
     fetchThemes(){
-
+      this.$store.state.label = "Gukwega ama karata yose ..."
+      axios.get(this.url+"/cartethematiques/").then(res => {
+        this.$store.state.themes = res.data.results
+        window.localStorage.setItem("themes", JSON.stringify(this.$store.state.themes))
+      }).catch(err => {
+        this.$store.state.label = ""
+        console.error(err);
+        this.$store.state.themes = JSON.parse(window.localStorage.getItem("themes"))
+      })
+    },
+    selectMap(){
+      map_chooser.click()
     }
   },
   mounted(){
@@ -46,5 +73,8 @@ export default {
   align-items: center;
   flex-direction: column;
   color: #555;
+}
+#map_chooser{
+  display: none;
 }
 </style>
